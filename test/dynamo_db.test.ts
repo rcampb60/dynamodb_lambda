2f2760a0c1a798c1b35c819d70a2e1aaebe8c08a
Robbie Campbell <robbiecam@gmail.com>
2023-08-11 20:25:28 +0100

    Initial commit

---
+++ test/dynamo_db.test.ts
@@ -0,0 +1,17 @@
+// import { Template } from '@aws-cdk/assertions';
+// import * as cdk from '@aws-cdk/core';
+// import * as DynamoDb from '../lib/dynamo_db-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/dynamo_db-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new DynamoDb.DynamoDbStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
